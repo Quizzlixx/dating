d0c735ac33efdfab5bbe33108046281aa4a2e0fe
Kerrie Low <klow4@mail.greenriver.edu>
2020-01-26 17:09:33 -0500

    Minor route render fixes

--- index.php
+++ index.php
@@ -16,11 +16,9 @@ error_reporting(E_ALL);
 require_once('vendor/autoload.php');
 
 // instantiate F3
-// :: means static method
 $f3 = Base::instance();
 
 // define a default route
-// -> calls an instance method
 $f3->route('GET /', function() {
     $view = new Template();
     echo $view->render('views/home.html');
@@ -32,14 +30,19 @@ $f3->route('GET /personal-information', function() {
     echo $view -> render('views/personal-information.html');
 });
 
-
 // profile route
 $f3->route('POST /profile', function() {
     var_dump($_POST);
     var_dump($_SESSION);
 
+    $_SESSION['fName'] = $_POST['fName'];
+    $_SESSION['lName'] = $_POST['lName'];
+    $_SESSION['age'] = $_POST['age'];
+    $_SESSION['gender'] = $_POST['gender'];
+    $_SESSION['phone'] = $_POST['phone'];
+
     $view = new Template();
-    echo $view -> render('views/personal-information.html');
+    echo $view -> render('views/profile.html');
 });
 
 // interests route
@@ -48,7 +51,7 @@ $f3->route('POST /interests', function() {
     var_dump($_SESSION);
 
     $view = new Template();
-    echo $view -> render('views/personal-information.html');
+    echo $view -> render('views/interests.html');
 });
 
 // run f3
