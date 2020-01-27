34bd99395c9be640e236e98eaac9db69c12ea050
Kerrie Low <klow4@mail.greenriver.edu>
2020-01-26 19:19:46 -0500

    Navbar altered to redirect to home when text is clicked. Summary page and view created. Interestes layout finished

--- index.php
+++ index.php
@@ -19,21 +19,21 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 // define a default route
-$f3->route('GET /', function() {
+$f3 -> route('GET /', function() {
     $view = new Template();
     echo $view->render('views/home.html');
 });
 
 // personal information route
-$f3->route('GET /personal-information', function() {
+$f3 -> route('GET /personal-information', function() {
     $view = new Template();
     echo $view -> render('views/personal-information.html');
 });
 
 // profile route
-$f3->route('POST /profile', function() {
+$f3 -> route('POST /profile', function() {
     var_dump($_POST);
-    var_dump($_SESSION);
+//    var_dump($_SESSION);
 
     $_SESSION['fName'] = $_POST['fName'];
     $_SESSION['lName'] = $_POST['lName'];
@@ -46,13 +46,33 @@ $f3->route('POST /profile', function() {
 });
 
 // interests route
-$f3->route('POST /interests', function() {
+$f3 -> route('POST /interests', function() {
     var_dump($_POST);
-    var_dump($_SESSION);
+//    var_dump($_SESSION);
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['state'] = $_POST['state'];
+    $_SESSION['seeking'] = $_POST['seeking'];
+    $_SESSION['size'] = $_POST['size'];
+    $_SESSION['vaccination'] = $_POST['vaccination'];
+    $_SESSION['pName'] = $_POST['pName'];
+    $_SESSION['species'] = $_POST['species'];
+    $_SESSION['biography'] = $_POST['biography'];
+
 
     $view = new Template();
     echo $view -> render('views/interests.html');
 });
 
+// summary route
+$f3 -> route('POST /summary', function() {
+    var_dump($_POST);
+//    var_dump($_SESSION);
+    $_SESSION['indoor[]'] = $_POST['indoor[]'];
+    $_SESSION['outdoor[]'] = $_POST['outdoor[]'];
+
+    $view = new Template();
+    echo $view -> render('views/summary.html');
+});
+
 // run f3
-$f3->run();
+$f3 -> run();
